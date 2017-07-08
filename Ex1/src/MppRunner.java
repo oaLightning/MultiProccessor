d9672c19859c58fcaaf3dfdc0ef4f0da3441fb1a
Omer Barak <omeramosbarak@gmail.com>
2017-07-08 18:05:12 +0300

    x

--- Ex1/src/MppRunner.java
+++ Ex1/src/MppRunner.java
@@ -3,7 +3,7 @@ import mpp.EX4Q7;
 public class MppRunner {
 	static final int TIMES_TO_RUN = 1; 
 	public static void main(String[] args) {
-		String path = "C:\\Temp\\graph2.txt";
+		String path = "/tmp/random-graph.txt";
 		for (int num_threads : new int[] {1, 2, 4, 8, /*16, 32, 64*/} ) {
 			for (int i = 0; i < TIMES_TO_RUN; i++) {
 				EX4Q7.main(num_threads, path);
