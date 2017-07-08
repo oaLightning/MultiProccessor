b11589fcffab90cdc8eb609bc9d574cefaee8895
Omer Barak <omeramosbarak@gmail.com>
2017-07-08 18:00:11 +0300

    Made another runner

--- Ex1/src/MppRunner_2.java
+++ Ex1/src/MppRunner_2.java
@@ -4,7 +4,7 @@ public class MppRunner {
 	static final int TIMES_TO_RUN = 1; 
 	public static void main(String[] args) {
 		String path = "C:\\Temp\\graph2.txt";
-		for (int num_threads : new int[] {1, 2, 4, 8, /*16, 32, 64*/} ) {
+		for (int num_threads : new int[] {/*1, 2, 4, 8, */16, 32, 64} ) {
 			for (int i = 0; i < TIMES_TO_RUN; i++) {
 				EX4Q7.main(num_threads, path);
 				System.gc();
